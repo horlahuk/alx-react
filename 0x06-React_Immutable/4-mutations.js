7ff0fa867ec236cd3f762e687487ebfe511d0b82
horlahuk <olaitan.u.k@gmail.com>
2024-01-24 15:28:23 +0100

    task 4 fix

--- 0x06-React_Immutable/4-mutations.js
+++ 0x06-React_Immutable/4-mutations.js
@@ -1,6 +1,6 @@
 import { Map } from 'immutable';
 
-const map = Map({
+export const map = Map({
   1: 'Liam',
   2: 'Noah',
   3: 'Elijah',
@@ -9,6 +9,6 @@ const map = Map({
   6: 'Lucas',
 });
 
-const map2 = map.withMutations((values) => {
+export const map2 = map.withMutations((values) => {
   values.set(2, 'Benjamin').set(4, 'Oliver');
 });
